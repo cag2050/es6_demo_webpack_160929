4c3f0ae335a79e012e74475aa56600939bcf7074
chenanguo <chenanguo@le.com>
2016-09-30 11:00:49 +0800

    add

--- es6/import_var.js
+++ es6/import_var.js
@@ -1,7 +1,7 @@
 /**
  * Created by chenanguo on 2016/9/30.
  */
-// webpack编译es6 module报错，尚不知原因
+// webpack编译es6 module报错，尚不知原因。export文件后缀修改为.jsx,就能编译通过
 /*
 ERROR in ./es6/import_var.js
 Module not found: Error: a dependency to an entry point is not allowed
